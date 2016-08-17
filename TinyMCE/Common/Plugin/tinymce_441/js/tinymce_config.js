563865a444a6b8d643764321d7563be1202d794c
Mason Briekroak <blackmason805@gmail.com>
2016-08-17 22:48:23 +0900

    TinyMCE Eval !!

---
+++ TinyMCE/Common/Plugin/tinymce_441/js/tinymce_config.js
@@ -0,0 +1,17 @@
+﻿tinymce.init({
+    selector: '#mytextarea',
+    width: 800,
+    height: 500,
+    toolbar: 'undo redo | styleselect bold italic | alignleft aligncenter alignright | bullist numlist | outdent indent | code image custom_image',
+    plugins: 'code image',
+    menubar: 'file edit insert view format tools',
+    setup: function (editor) {
+        editor.addButton('custom_image', {
+            title: 'Custom Image',
+            icon: 'image',
+            onclick: function () {
+                window.open('http://www.google.com','Attach Images','width=600,height=400')
+            }
+        })
+    }
+});
---
+++ TinyMCE/Controllers/BBSController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinyMCE.Controllers
+{
+    public class BBSController : Controller
+    {
+        //
+        // GET: /BBS/
+
+        public ActionResult Default()
+        {
+            return View();
+        }
+
+    }
+}
